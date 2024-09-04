5fcb0c96ae475abd2d8ecc647a9e7a4b746754a2
mkagan <michaelkagan1994@gmail.com>
2024-09-04 16:45:57 -0700

    added readme markdown file and requirements txt file for dependencies

---
+++ README.md
@@ -0,0 +1,51 @@
+#Salsa Song Recs
+
+This flask-based python app allows a user to discover salsa songs at a desired speed/tempo/bpm. The app uses the Spotify API and uses a searched song as the seed query for finding more recommendations. 
+
+##User and GUI Overview
+1. The first thing a user sees when navigating to my app is a blank, dark themed page with a Spotify logo at the bottom and a disabled search bar. 
+2. The login button routes users through to the spotify account login and scope permissions checkpoint. 
+3. After logging in and granting permission, a user can enter a song into the search field. 
+4. The results page shows a table of the top 10 track results for the user's song query. On the right, an embedded spotify player is loaded with the first song in the list. A user can preview songs by clicking the play button in the results table, which loads that song into the embedded player. A user can not use the embedded player like a standalone spotify app. Each song in the results has an option to *Get recs*. 
+5. The final recs - recommendations - page delivers up to 50 song recommendations based on the original queried song. They are found by matching the main criterium, which is target tempo, as well as using the seed song and seed genre - salsa.
+6. Along with the recs table, there is a similar embedded spotify player on the right of the screen. The new element is a set of page controls at the bottom of songs list. Each page contains up to 10 songs.
+
+The navbar at the top has a button to log in. My app uses the spotify api, therefor it requires users to go through the OATH2.0 authorization. 
+
+##File Overview
+Here is a list of files with a brief description in my application folder:
+- app.py	(main flask app controller, with all routes, and all api call functions)
+- config.py	(not included - helper config app, containing app secrets, such as secret key, and token information, as well as endpoint uris, and sqlite database initiation)
+- songs.db	(sqlite3 database used to store recommended song request response. db was overwritten everytime a query was made) 
+- authorizeme.py	(helper python file with my functions used for generating random, url-save strings used for tokens and secrets, as well as hashing said strings)
+- requirements.txt	(txt file containing necessary libraries for program to run)
+- templates/
+	- layout.html	(General layout html with 
+	- home.html
+	- header.html
+	- results.html
+	- player.html
+	- recs.html
+	- pagination.html
+- static/
+	- spotify.png
+
+
+##Challenges
+
+This project was a huge learning process for me every step of the way. However it included a few particularly difficult roadblocks that took the most time to figure out. 
+
+####OATH2.0 & hashing
+Authorization was tricky for several reasons. Everytime I thought I found ***the*** bug, it turned out there was another, yet undiscovered one lurking somewhere behind it. Of these issues, the random string hashing and base64 encoding was the trickiest. However, even when I was sure I had that figured out, I couldn't proceed until I solved the issue with...
+
+####Safary privacy restrictions
+Yes, safari privacy restrictions. After spending well over 2 days reading the spotify api documentation, watching youtube tutorials, rewriting code and fruitless conversations with my best friend, chatGPT, I decided to run the flask app on google chrome instead. ***poof*** It worked. It turns out, incognito mode in safari was restricting certain functionality (I still don't know what), but chrome incognito mode did not. Only much later did I realize, I can in fact run my app in safari, just not incognito. 
+
+####Pagination
+The trouble with coding the pagination for me was 3-fold. 
+
+1. The actual algorithm for dynamically generating the page buttons, while constraining them to 3 buttons max, never exceeding the range of 1 to the final page of songs. This was a problem because I didn't know whether the ideal solution should be coded in my flask app (in conjuction with Jinja templating), in my html code as a JS script (appending html children), or as a separate JS helper file. The solution to this was simple in the end - I used a helper python function in my flask app. Given a target page, it returned the indeces of the starting and ending pages, as well as the starting and ending songs in the display page This function was less than 20 lines. I chose this path, because I was most familiar with python, and a friend made me realize - that was the reason for flask in the first place. 
+2.  Pagination made me face the issue of saving the songs somehow. If not, the app might re-send api requests to spotify and reload my recs list, potentially with new songs, so I wouldn't have a static results table. I never tried this, but this was my suspicion. This led me down the path of making a simple sqlite database, which I used to save recommendation responses and generate new pages and song lists. Every time a new post request is made to my *"/recs"* route, the db is wiped and repopulated with the new results.
+3. Centering my page buttons! This was a trivial but important lessson in basic html styling that took longer for me to learn that I would have liked. Moral of the story: when you have multiple nested tags and html elements, bootstrap classes can and will easily interfere with eachother. It's important to backtrack, even across templates to make sure that only the necessary element parent has the needed styling class. In my case, I had redundant classes for flexbox, or for centering my text, even across 2 html templates. This was resolved when I simplified my code.
+
+
--- app.py
+++ app.py
@@ -4,8 +4,6 @@ adapted to python, flask
 
 Michael Kagan 2024
 """
-import pdb
-
 from flask import Flask, render_template, redirect, request, session, jsonify
 from flask_session import Session
 from datetime import datetime
@@ -14,16 +12,8 @@ import requests, os, math, time
 from config import *
 
 app = Flask(__name__)
-app.config.from_pyfile("config.py")
+app.config.from_pyfile("config.py")	#config.py contains: app-secret-key, client id/secret, session configs, code verifier/challenge, endpoint URIs, & database uri
 Session(app)
-
-#Setup sqlite database (in config)
-
-#Set up constants for authorization data -- Adapted from "Spotify API OAuth - Automate Getting User Playlists (Complete Tutorial)" by Imdad Codes youtube video "aka: VIDEO"
-REDIRECT_URI = "http://localhost:8000/callback"
-AUTH_URL = "https://accounts.spotify.com/authorize"
-TOKEN_URL = "https://accounts.spotify.com/api/token"
-API_BASE_URL = "https://api.spotify.com/v1/"
 
 @app.route("/", methods=["GET","POST"])
 def index():
@@ -31,28 +21,28 @@ def index():
 
 @app.route("/login")
 def login(): 	#Adapted from Spotify documentation
-	if request.method == "GET":
-		scopes  = ['user-modify-playback-state', 'streaming', 'playlist-modify-private', 'playlist-modify-public']      #List of all desired scopes for application, including adding/modifying
-                	                                                                                                        #playlists and allowing music playback.
-		scope = ' '.join(scopes)        #Concatenate string of scopes joining by a space.
+	scopes  = ['user-modify-playback-state', 'streaming', 'playlist-modify-private', 'playlist-modify-public']      #List of all desired scopes for application, including adding/modifying
+															#playlists and allowing music playback.
+	scope = ' '.join(scopes)        #Concatenate string of scopes joining by a space.
 
-		params = { 
-			"client_id": CLIENT_ID, 
-			"response_type": "code",
-			"redirect_uri": REDIRECT_URI,       #must be included in app details in spotify developer page
-			"state": STATE,
-			"scope": scope,
-			"code_challenge_method": "S256",
-			"code_challenge": CODE_CHALLENGE,	#added trailing comma to test debugging. matching documentation, doubt it'll help
-			}   
-		
-		auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"	#adapted from VIDEO
-		auth_url = auth_url.replace('+','%20')				#+s in url string changed to %20. Might not be necessary, but gave it a shot in debugging
-		return redirect(auth_url)
+	params = { 
+		"client_id": CLIENT_ID, 
+		"response_type": "code",
+		"redirect_uri": REDIRECT_URI,       #must be included in app details in spotify developer page
+		"state": STATE,
+		"scope": scope,
+		"code_challenge_method": "S256",
+		"code_challenge": CODE_CHALLENGE,	#added trailing comma to test debugging. matching documentation, doubt it'll help
+		}   
+	
+	auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"	#adapted from VIDEO
+	auth_url = auth_url.replace('+','%20')				#+s in url string changed to %20. Might not be necessary, but gave it a shot in debugging
+	return redirect(auth_url)
 
 @app.route("/logout")
 def logout():
 	session.clear()
+	db.execute("DELETE FROM songs")	#remove previously queried data in songs database 
 	return redirect("/")
 
 @app.route("/callback", methods=["GET","POST"])
@@ -60,24 +50,23 @@ def callback():
 
 	#check for error by not presence of "code" in the callback url 
 	if 'error' in request.args:
-		return request.args.get("error")
-	
+		return render_template("layout.html", error=request.args.get("error"))
+
 	if STATE != request.args.get("state"):	#state code returned from Spotify server during authorization request must match saved STATE value from constants
-		#return "state values do not match"
-		return "State value mismatch!"
+		return render_template("layout.html", error="State value mismatch!")
 
 	if 'code' in request.args:
 		authorization_code = request.args.get("code")	#authorization code returned from Spotify server during auth request
 
 		params = {
-				"grant_type": "authorization_code",
-				"code": authorization_code,
-				"redirect_uri": REDIRECT_URI,
-				"client_id": CLIENT_ID,
-				"code_verifier": CODE_VERIFIER
+			"grant_type": "authorization_code",
+			"code": authorization_code,
+			"redirect_uri": REDIRECT_URI,
+			"client_id": CLIENT_ID,
+			"code_verifier": CODE_VERIFIER
 			}
 		headers = { 
-				"Content-Type": "application/x-www-form-urlencoded"
+			"Content-Type": "application/x-www-form-urlencoded"
 	     		}
 		
 		#Submit post request to token url and save response
@@ -102,13 +91,13 @@ def refresh_token():
 		return redirect("/home")
 
 	params = {
-			"grant_type": "refresh_token",
-			"refresh_token": session['refresh_token'],
-			"client_id": CLIENT_ID
+		"grant_type": "refresh_token",
+		"refresh_token": session['refresh_token'],
+		"client_id": CLIENT_ID
 		}
 
 	headers = {
-			"Content-Type": "application/x-www-form-urlencoded",
+		"Content-Type": "application/x-www-form-urlencoded",
 		}
 
 	response = requests.post(TOKEN_URL, data=params, headers=headers)	#Obtain response from post request
@@ -150,6 +139,12 @@ def home():
 
 @app.route("/recs", methods=['GET','POST'])
 def recs():
+	#Check for access_token precence and validity
+	if 'access_token' not in session:
+			return redirect("/login")
+	if datetime.now().timestamp() > session['expiration']:
+		return redirect("/refresh-token")
+
 	if request.method == 'POST':
 		path = request.form.get("request_type")	#This is the name label on the recs list buttons. 
 		song_id = request.form.get("song_id")
@@ -189,6 +184,14 @@ def page_route(page):
 		return render_template("recs.html", songs=songs[songi:songf], page=page, pagei=pagei, pagef=pagef)
 	else:
 		return render_template("layout.html", error="Page out of range!")
+
+
+
+""" 
+
+Helper functions for api calls defined below 
+
+"""
 
 
 def song_search(ACCESS_TOKEN, song_title, limit=10):
@@ -294,6 +297,7 @@ def pagination(page):
 	songf = min( (songi + 10) ,num_songs-1)	#final song, either initial song + 10 or the last song, whichever is smaller
 
 	return songi, songf, pagei, pagef	#returns all 4 numbers to the route page rendering. Used in jinja2 loop in the html to generate the relevant song list and page links
-
+"""
 if __name__ == "__main__":
     app.run(port=8000, debug=False)
+    """
